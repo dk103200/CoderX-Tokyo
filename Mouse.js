a929579a5222ec0dadb79a3fef06aa6ebda2103f
PDThanh <dk103200.gmail.com>
2020-09-09 21:58:05 +0700

    modify mouse

--- Mouse.js
+++ Mouse.js
@@ -1,5 +1,14 @@
-function Mouse(){
+function Mouse(name){
+	this.name = name
+	this.isDead
 
+}
+Mouse.prototype.die = function() {
+	this.isDead  true	
+};
+
+Mouse.prototype.run = function(){
+	console.log('Run')
 }
 
 module.exports = Mouse
